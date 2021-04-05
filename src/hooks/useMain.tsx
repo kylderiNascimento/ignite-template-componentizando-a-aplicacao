26fb6b1799f98835d675c659ff10be4f6255b33a
kylderinascimento <kylderi.nascimento@hotmail.com>
2021-04-05 02:10:01 -0300

    Challenge Finalized

---
+++ src/hooks/useMain.tsx
@@ -0,0 +1,38 @@
+import { useContext, ReactNode, useState, createContext } from 'react';
+
+// type para o context
+interface MainContextData {
+    selectedGenreId: number;
+    handleClickButton: (id: number) => void;
+}
+
+interface MainProviderProps {
+    children: ReactNode;
+}
+
+// inicializando o context de
+const MainContext = createContext<MainContextData>({} as MainContextData);
+
+export function MainProvider({children}: MainProviderProps){
+
+    const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+    function handleClickButton(id: number) {
+        setSelectedGenreId(id);
+    }
+
+    return (
+        <MainContext.Provider value={{ selectedGenreId, handleClickButton}} >
+            { children } 
+        </MainContext.Provider>
+    );
+}
+
+
+
+export function useMain(){
+    const context = useContext(MainContext);
+
+    return context;
+}
+
